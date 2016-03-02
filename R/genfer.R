4d30a73bbf2ed3c94632f2596c29179256ed249c
Germán Rodríguez <grodri@princeton.edu>
2016-03-02 17:57:36 -0500

    Fixed genfer export and args

--- R/genfer.R
+++ R/genfer.R
@@ -6,15 +6,17 @@
 #' @param control scalar representing degree of control (m)
 #' @param mean scalar representing mean age of entry into union
 #' @param stdev scalar representing std. dev. of age at union
-genfer <- function(age, level=1, control=20, mean, stdev) {
+#' @export
+genfer <- function(age, mean, stdev, level, control) {
   if(level <= 0) {
     stop("Level parameter must be non-negative")
   }
   if(mean <= 0) {
     stop("Mean age of entry into union must be no-negative")
   }
-  if(stdve <= 0) {
+  if(stdev <= 0) {
     stop("Standard deviation of age of entry into union must be no-negative")
   }
   marfer(age, level, control) * pnupt(age, mean, stdev, 1)
 }
+
